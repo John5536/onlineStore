5243722df23a49e7d1888bcf04b3d583ec3be4e0
john <rrivers52@gmail.com>
2025-01-07 11:31:53 -0500

    assignment 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Footer from '.components/footer';
+import Catalog from'.components/catalog';
+import Home from '.components/home';
+import About from '.components/about';
+
+import { browserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-       <Navbar></Navbar>
-       <h1>Hello world from react</h1>
-       <Catalog></Catalog>
-       <Product></Product>
-       <Product></Product>
-       <Product></Product>
-       <Product></Product>
+    
+      <browserRouter>
+      <div className="App">
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/catalog" element={<Catalog/>} />
+        <Route path="/about" element={<About/>} />
+
+      </Routes>
        <Footer></Footer>
+       </div>
+      </browserRouter>
+       
+      
        
        
       
-    </div>
+   
     
   );
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,12 @@
+import "./styles/about.css";
+
+function About(){
+    return(
+        <div className="about">
+            <h1>A little about me</h1>
+            <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem ipsum cumque quibusdam deserunt ullam dolore quae hic corporis accusantium nesciunt rem maxime in, beatae sed magnam repudiandae cum, commodi nulla.</h3>
+        </div>
+    );
+}
+
+export default Abaut;
