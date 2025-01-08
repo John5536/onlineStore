ca3ed4ae5d62222dfe5e54f319cd2f5b5ca9fd7b
john <rrivers52@gmail.com>
2025-01-08 15:05:36 -0500

    final project

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,16 +1,46 @@
 import Product from "./product";
 import "./styles/catalog.css";
 import Product from "./product";
+import{useState} from 'react'
+
+const dummyData = [
+   {"title": "orange",
+    "category":  "fruit",
+    "price": 15.99,
+    "image": "/images/oranges.png",
+    "_id": "45",
+   },
+   {"title": "strawberry",
+    "category":  "fruit",
+    "price": 4.90,
+    "image": "/images/strawberry.png",
+    "_id": "49",
+   },
+   {"title": "melon",
+    "category":  "fruit",
+    "price": 10.67,
+    "image": "/images/melon.png",
+    "_id": "40",
+   }
+    
+];
+
+const dummyCategories = ["fruit", "vegetables","beverages", "meat"]
 
 function Catalog(){
+    const [allProducts ,setAllProducts] = useState(dummyData);
+    const [categories, setCategories] = useState(dummyCategories);
     return(
         <div className="catalog page">
             <h1>Check out the amazing catalog</h1>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
+            <div className="filter">
+            {categories.map(cat => <button className="btn btn-sm btn-primary">{cat}</button>)}
+            </div>
+            {
+                allProducts.map( prod => <Product data={prod} />)
+            }
+            
         </div>
-    )
+    );
 }
 export default Catalog;
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,13 +1,15 @@
 import QuantityPicker from "./quantityPicker";
 import "./styles/product.css"
-function Product(){
+function Product(props){
     return (
         
             <div className="product">
-                <img src="https://picsum.photos/200" alt="" />
-                <h3>Product 1</h3>
+                <img src={props.data.image} alt="" />
+                <h3>{props.data.title}</h3>
 
                 <label>99.99</label>
+                <label>${props.data.price}</label>
+
                 <QuantityPicker></QuantityPicker>
             </div>
             
